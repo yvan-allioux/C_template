83b899cf894c92030fe711223c881f0f0102a2c7
Yvan Allioux <yvan.allioux.pro@gmail.com>
2022-03-20 23:52:02 +0100

    test helloword

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+int usleep(useconds_t useconds);
+
+void cube(){
+    const int MAX = 100, MIN = 1;
+    int i = 0;
+    while (i != 100)
+    {
+      
+            int i2 = 0;
+            while (i2 != i)
+            {
+                printf(" ");
+                i2++;
+            }
+
+        
+        printf("%d\n", i);
+        //printf("data %d\n", i);
+        usleep(10000);
+        i++;
+    }
+    
+}
 
 int main()
 {
     printf("hello world !\n");
+    cube();
 
     return 0;//0 success, 1 (ou any non-0) failure
 }
--- parametre_de_lancement/main.c
+++ parametre_de_lancement/main.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/ipc.h>
-#include <sys/sem.h>
 #include <stdlib.h>
 
 
